27090b4c30b59e80d86c065925b05e5a85d5fa6e
sb-raza <syedrazatx@gmail.com>
2019-09-29 15:31:27 -0500

    Added functionality to get images from google images

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import imagescraper
+import imagescraper_google
 
-imagescraper.get_images("https://www.google.com")
+init_question = input('Extract images from (S)ingle page or (G)oogle images > ')
+
+if init_question.lower() == "s":
+    url = input('Enter url for page (without http) > ')
+    imagescraper.get_images('http://' + url)
+
+elif init_question.lower() == "g":
+    base_url = 'https://www.google.com/search?safe=strict&tbm=isch&q='
+    url = input('Enter search string for Google Images > ')
+    quant = input('Number of pictures to pull > ')
+    imagescraper_google.get_images(base_url+url, quant)
+
+else:
+    print('Invalid Input')
 
 print("Done")
---
+++ imagescraper_google.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+from pathlib import Path
+
+
+def get_images(url, number_of_imgs):
+    # Open url
+    r = requests.Session()
+
+    res = r.get(url)
+    res.raise_for_status()
+
+    # Parse result to BeautifulSoup
+    soup = BeautifulSoup(res.content, 'html.parser')
+
+    # Find all <img> tags
+    images = soup.findAll('img')
+
+    # Iterate through <img> tags to extract image src
+    i = 0
+    for image in images[0:int(number_of_imgs)]:
+        i += 1
+        image_url = image.get('src')
+        image_name = f'img {str(i)}.jpg'
+        image_data = r.get(image_url)
+        image_data.raise_for_status()
+
+        folder = Path('temp/')
+        folder.mkdir(parents=True, exist_ok=True)
+        filepath = folder / image_name
+
+        with filepath.open('wb') as file:
+            for chunk in image_data.iter_content(100000):
+                file.write(chunk)
